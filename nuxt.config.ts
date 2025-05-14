faa6d5af358ae0662b170307e811057daa748758
Pierre-Alexandre <pa.martin@outlook.fr>
2025-05-14 09:39:14 +0200

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/icon',
+    '@nuxt/ui',
+    '@nuxt/test-utils',
+    '@nuxt/scripts',
+    '@nuxt/image',
+    '@nuxt/fonts',
+    '@nuxt/content'
+  ]
+})
